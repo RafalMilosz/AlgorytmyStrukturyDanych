602d59f63c54855d80266bcefc53cb31071d638f
RafalMilosz <115027153+RafalMilosz@users.noreply.github.com>
2023-02-12 16:00:01 +0200

    Create zad7.c

---
+++ lab/laboratorium/lab1/zad7.c
@@ -0,0 +1,31 @@
+/*
+Poszukiwanie minimalnej albo maksymalnej.
+*/
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+void main(void){
+  srand(time(NULL));
+  
+  int n = 20;
+  double x[n], min = 1000, max = 0;
+
+  for(int i = 0; i < n; i++){
+    x[i] = (rand() % 1000) + 1;
+  } 
+  
+  for(int i = 0; i < n; i++){
+    if(min > x[i]){
+      min = x[i];
+    }
+    if(max < x[i]){
+      max = x[i];
+    }
+    }
+
+  printf("Minimum = %lf", min);
+  printf("Maximum = %lf", max);
+}
