08fbbd5c7e2739595f6f051b965ad3b83b2e2b8c
RafalMilosz <115027153+RafalMilosz@users.noreply.github.com>
2023-02-12 17:34:54 +0200

    Create zad4.c

---
+++ lab/laboratorium/lab7/zad4.c
@@ -0,0 +1,31 @@
+/*
+ Wyprowadzić wszystkie nie powtarzające się słowa
+*/
+#include <stdio.h>
+#define MAX_LINE_LENGTH 1000
+#define LETTERS_COUNT 256
+int main(void)
+{
+    
+    char str[MAX_LINE_LENGTH];
+    int i;
+    int freq[LETTERS_COUNT] = {0};
+    printf(" Program do wyszukiwania wszystkich nie powtarzających się znaków \n");
+    printf("\nWprowadź ciąg: ");
+    scanf("%[^\n]",str);
+    
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[(int)str[i]]++;
+    }
+    printf("Postacie, które nie mają powtórzeń są: \n");
+    for(i = 0; i < 256; i++)
+    {
+      
+        if(freq[i] == 1)
+        {
+            printf("%c ", i);
+        }
+    }
+    return 0;
+}
