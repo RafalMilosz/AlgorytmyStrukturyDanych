3f08d4ba263e2713bb740d302ea4d9b05a800731
RafalMilosz <115027153+RafalMilosz@users.noreply.github.com>
2023-02-12 16:30:51 +0200

    Create zad3.c

---
+++ lab/laboratorium/lab4/zad3.c
@@ -0,0 +1,22 @@
+/*
+Napisz funkcja rekurencyjna do obliczenia n-tej liczby ciągu arytmcznego
+*/
+#include <stdio.h>
+
+int arytm(int a, int d, int n);
+
+int main(void) {
+  printf("Hello World\n");
+  int a = 1;
+  int d = 3;
+  int n = 7;
+  printf("Arytm is: %d\n", arytm(a, d, n));
+  return 0;
+}
+
+int arytm(int a, int d, int n) {
+  if (n == 1) {
+    return a;
+  }
+  return arytm(a, d, n - 1) + d;
+}
